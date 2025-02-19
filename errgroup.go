60d9ca094a31c762751ff9bc50bd6856682e5224
sysradium <sysradium@users.noreply.github.com>
2025-02-19 12:27:20 +0000

    feat: Add errgroup implementation for managing goroutines

---
+++ errgroup.go
@@ -0,0 +1,49 @@
+package errgroup
+
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type Group struct {
+	*errgroup.Group
+	middlewares []Middleware
+}
+
+func (g *Group) Go(fn GFn) {
+	res := fn
+	for i := len(g.middlewares) - 1; i >= 0; i-- {
+		res = g.middlewares[i](res)
+	}
+
+	g.Group.Go(res)
+}
+
+// can the middleware unwrap part be refactroed using built-in Golang tools? AI!
+func (g *Group) TryGo(fn GFn) bool {
+	res := fn
+	for i := len(g.middlewares) - 1; i >= 0; i-- {
+		res = g.middlewares[i](res)
+	}
+
+	return g.Group.TryGo(res)
+}
+
+func New(middlewares []Middleware) *Group {
+	return &Group{
+		Group:       &errgroup.Group{},
+		middlewares: middlewares,
+	}
+}
+
+func WithContext(ctx context.Context) (Group, context.Context) {
+	g, ctx := errgroup.WithContext(ctx)
+	return Group{
+		Group: g,
+	}, ctx
+}
+
+type GFn func() error
+
+type Middleware func(GFn) GFn
